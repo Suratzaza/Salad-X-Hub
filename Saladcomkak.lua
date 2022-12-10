d3ef45db39ab985bb3c815b2365ddf8d9cece886
Suratzaza <120245703+Suratzaza@users.noreply.github.com>
2022-12-10 15:43:19 +0700

    Create Saladcomkak.lua

---
+++ Saladcomkak.lua
@@ -0,0 +1,45 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Salad X Hub             สคริปฟรีแต่ถ้าใช้อันนึงแล้วต้องออกมากดเอาใหม่เด้ออCr:Xqsrrrr", "Synapse")
+local Tab = Window:NewTab("หมวดหมู่สคริป")
+local Section = Tab:NewSection("Unique Hub")
+Section:NewButton("คลิกเพื่อใช้", "สคริปUique Hub", function()
+    print("Clicked")loadstring(game:HttpGet("https://raw.githubusercontent.com/AkiraNus/UniquehubKak/main/FreeCr.Xenonhub"))(); 
+end)
+
+
+local Section = Tab:NewSection("เลเวลปลอมเอาไว้หลอกเพื่อน")
+Section:NewButton("คลิกเพื่อใช้", "สคริปแกล้งเพื่อน", function()
+    print("Clicked")loadstring(game:HttpGet('https://raw.githubusercontent.com/SHAREHACK/allscript/main/level'))()
+end)
+
+
+local Section = Tab:NewSection("Zen Hub MB/PC")
+Section:NewButton("คลิกเพื่อใช้", "สคริปเล่นได้ทั้งคอมและมือถือ", function()
+    print("Clicked")loadstring(game:HttpGet("https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader", true))()
+end)
+
+
+local Section = Tab:NewSection("Thunder Z ไก่ตันตึงๆ")
+Section:NewButton("คลิกเพื่อใช้", "ไก่ตันตึงๆ", function()
+    print("Clicked")loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-05/HUB/main/Kaitun"))()
+end)
+
+
+local Section = Tab:NewSection("Viper hub ซอยยิบๆ")
+Section:NewButton("คลิกเพื่อใช้", "ซอยยิบๆ", function()
+    print("Clicked")if not syn then getgenv().syn={queue_on_teleport=queue_on_teleport or function()end}end loadstring(game:HttpGet("https://raw.githubusercontent.com/onehalla/1hallascripts/master/release/viperhub.lua", true))()
+end)
+
+
+local Section = Tab:NewSection("Mukuro Hub ตึงๆ")
+Section:NewButton("คลิกเพื่อใช้", "ฟาร์มโครตดีย์", function()
+    print("Clicked")loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+end)
+
+
+local Section = Tab:NewSection("Ripper Hub ในตำนาน")
+Section:NewButton("คลิกเพื่อใช้", "สคริปในตำนาน", function()
+    print("Clicked")loadstring(game:HttpGet('https://raw.githubusercontent.com/xDestinyx/RipperHub/main/Loader.lua'))();
+end)
+ Section = Tab:NewSection("Comming Soon")
+
